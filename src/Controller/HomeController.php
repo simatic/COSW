83a0b15c25c537adb3302e7301b38bb44684323d
Gib <gibril.gunder@telecom-sudparis.eu>
2021-05-10 17:47:56 +0200

    Retour à un modèle à plusieurs contrôleurs

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller;
+
+// Required for a controller.
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+// Requests and responses handling
+use Symfony\Component\HttpFoundation\Response;
+
+// Route annotations
+use Symfony\Component\Routing\Annotation\Route;
+
+// Other dependencies
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+/**
+* Ce contrôleur gère les routes suivantes :
+* Notation : <chemin> : "<nom_de_la_route>" (<explications>)
+* 
+* / : "home" (La page d'accueil du site)
+*
+* /login : "login" (Pour se connecter [seulement pour les organisateurs de soutenances et les administrateurs de COS])
+*
+* /logout : "logout" (Pour se déconnecter)
+*/
+class RegistrationController extends AbstractController {
+
+    /**
+     * @Route("", name="home")
+     */
+    public function home(): Response {
+        
+        return $this->render('home/index.html.twig');
+
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(String $type, AuthenticationUtils $authenticationUtils): Response {
+
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error, 'type' => $type]);
+
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout() {
+
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        
+    }
+
+}
---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Controller;
+
+// Required for a controller.
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+// Requests and responses handling
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+// Route annotations
+use Symfony\Component\Routing\Annotation\Route;
+
+// Other dependencies.
+use App\Entity\Creator;
+use App\Form\CreatorType;
+use App\Entity\AccountRequest;
+use App\Form\AccountRequestType;
+use App\Repository\AccountRequestRepository;
+use App\Security\Status;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+/**
+* Ce contrôleur gère les routes suivantes : (toutes les routes commençant par "/register")
+* Notation : <chemin> : "<nom_de_la_route>" (<explications>)
+* 
+* /register : "register" (Pour faire une demande de création de compte organisateur)
+*
+* /register/complete-registration : "complete_registration" (Pour finaliser la création d'un compte organisateur 
+* un fois que la demande a été validée. L'utilisateur finalise la création de son compte en définissant un 
+* mot de passe.)
+* -------------------------------------
+*
+* @Route("/register", name="register_")
+*/
+class RegistrationController extends AbstractController {
+
+    /**
+     * @Route("", name="register")
+     */
+    public function register(Request $request, AccountRequestRepository $accountRequestRepository): Response {
+        
+        $accountRequest = new AccountRequest();
+
+        $form = $this->createForm(AccountRequestType::class, $accountRequest);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            if($accountRequestRepository->findBy(['email' => $form->get("email")->getData()])) {throw new \Exception("Une demande a déjà été formulée pour cette adresse mail.");}
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($accountRequest);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('home');
+
+        }
+
+        return $this->render('account_request/new.html.twig', ['form' => $form->createView(), 'button_label' => 'Envoyer ma demande']);
+
+    }
+
+    /**
+     * @Route("/complete-registration/{id}", name="complete_registration")
+     */
+    public function completeRegistration(Request $request, AccountRequest $accountRequest, UserPasswordEncoderInterface $encoder) {
+        
+        if($accountRequest->getStatus() != Status::VALIDATED) {
+
+            return $this->render('account_request/not_yet_validated.html.twig');
+
+        }
+
+        $creator = new Creator();
+
+        $creator->setFirstName($accountRequest->getFirstName());
+        $creator->setLastName($accountRequest->getLastName());
+        $creator->setEmail($accountRequest->getEmail());
+
+        $form = $this->createForm(CreatorType::class, $creator);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $creator->setPassword($encoder->encodePassword($creator, $form->get('password')->getData()));
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($creator);
+            $entityManager->remove($accountRequest);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('home');
+
+        }
+
+        return $this->render('account_request/_form.html.twig', ['form' => $form->createView(), 'button_label' => 'Valider']);
+
+    }
+
+}
