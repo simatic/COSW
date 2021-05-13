f99558e1808b2ae09102071676ec50fdf4956b3a
Gib <gibril.gunder@telecom-sudparis.eu>
2021-05-13 18:18:19 +0200

    Modification du constructeur de Creator pour attribuer le rôle ROLE_CREATOR à la nouvelle instance créée

--- src/Entity/Creator.php
+++ src/Entity/Creator.php
@@ -2,15 +2,24 @@
 
 namespace App\Entity;
 
+// Routes annotations
 use Doctrine\ORM\Mapping as ORM;
+
+// Other dependencies
+use App\Security\Role;
 
 /**
  * @ORM\Entity
  * 
- * Class for orals organizers/creators
+ * Classe représentant les organisateurs de soutenances.
  */
 class Creator extends LoggedUser {
 
-    public function __construct() {parent::__construct();}
+    public function __construct() {
+        
+        parent::__construct();
+        $this->addRole(Role::CREATOR);
+    
+    }
 
 }
