b5d5e6a54263465206d29ad7691910f5ee81c12d
Gib <gibril.gunder@telecom-sudparis.eu>
2021-05-13 18:23:36 +0200

    Ajout des rôles ROLE_PEER et ROLE_JURY dans Role.php; remplacent ROLE_GUEST

--- src/Security/Role.php
+++ src/Security/Role.php
@@ -10,7 +10,8 @@ abstract class Role {
 
     public const USER = "ROLE_USER";
     public const ADMIN = "ROLE_ADMIN";
-    public const GUEST = "ROLE_GUEST";
+    public const PEER = "ROLE_PEER";
+    public const JURY = "ROLE_JURY";
     public const CREATOR = "ROLE_CREATOR";
 
 }
