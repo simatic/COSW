28acf300c0d889877d17e8e6f68e773b2b6a9832
Gib <gibril.gunder@telecom-sudparis.eu>
2021-05-13 18:26:36 +0200

    Documentation de Role.php

--- src/Security/Role.php
+++ src/Security/Role.php
@@ -8,10 +8,15 @@ namespace App\Security;
  */
 abstract class Role {
 
+    // Rôle par défaut de toute personne utilisant l'application COS.
     public const USER = "ROLE_USER";
+    // Les administrateurs de COS (valident les demandes de création de comptes d'organisateur de soutenances).
     public const ADMIN = "ROLE_ADMIN";
+    // Les pairs/étudiants.
     public const PEER = "ROLE_PEER";
+    // Les membres de jury.
     public const JURY = "ROLE_JURY";
+    // Les organisateurs de soutenances.
     public const CREATOR = "ROLE_CREATOR";
 
 }
