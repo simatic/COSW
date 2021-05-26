bcad369b0d2eab12353e214c680791d05c7b6f6e
Gib <gibril.gunder@telecom-sudparis.eu>
2021-05-26 16:47:16 +0200

    Ajout de l'entité "Grading" permettant aux utilisateurs d'évaluer les évaluables (items, rubriques, et fiches d'évaluation)

---
+++ src/Entity/Grading.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Entity;
+
+/**
+ * 
+ * Classe représentant l'évaluation d'un évaluable 
+ * (instances de "Gradable") par un utilisateur.
+ * Il existe au plus une instance de Grading par couple 
+ * ($gradable, $user).
+ */
+class Grading {
+
+    /**
+     * 
+     * L'évaluable évalué.
+     */
+    private Gradable $gradable;
+
+    /**
+     * 
+     * L'utilisateur ayant réalisé 
+     * l'évaluation.
+     */
+    private User $user;
+
+    /**
+     * 
+     * La note donnée à l'évaluable.
+     */
+    private float $grade;
+
+    /**
+     * 
+     * Un commentaire optionnel accompagnant 
+     * la note.
+     */
+    private string $comment;
+
+    public function __construct()
+    {
+        
+    }
+
+}
